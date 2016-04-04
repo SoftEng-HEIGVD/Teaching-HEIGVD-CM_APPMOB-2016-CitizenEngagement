84a68a383bdefe5cf24eecb6b6bb64144178e095
Flavia Pittet <pittet.flavia@gmail.com>
2016-04-04 15:59:42 +0200

    testing in class - not working

--- www/js/app.js
+++ www/js/app.js
@@ -3,22 +3,63 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('citizen-engagement', ['ionic'])
+angular.module('citizen-engagement', ['citizen-engagement.auth', 'ionic', 'citizen-engagement.constants'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+
+    /*function ContentController($scope, $ionicSideMenuDelegate) {
+     $scope.toggleLeft = function() {
+     $ionicSideMenuDelegate.toggleLeft();
+     };
+     }// TODO*/
+
+
+    .state('login', {
+        url: '/login',
+        controller: 'LoginCtrl',
+        templateUrl: 'templates/login.html'
+    })
+
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .run(function (AuthService, $rootScope, $state) {
+
+        // Listen for the $stateChangeStart event of AngularUI Router.
+        // This event indicates that we are transitioning to a new state.
+        // We have the possibility to cancel the transition in the callback function.
+        $rootScope.$on('$stateChangeStart', function (event, toState) {
+
+            // If the user is not logged in and is trying to access another state than "login"...
+            if (!AuthService.currentUserId && toState.name != 'login') {
+
+                // ... then cancel the transition and go to the "login" state instead.
+                event.preventDefault();
+                $state.go('login');
+            }
+        });
+    })
+
+
+/*
+.controller('Testcontroller', function($http){
+    $http({
+        url:'http://localhost:'
+    })
+})*/
