16bf5d8d28aefd8d92fcae04793ce00c2ad4bfba
Yann Longchamp <yann.longchamp@heig-vd.ch>
2016-04-04 15:37:34 +0200

    Templates, route, banner

--- www/js/app.js
+++ www/js/app.js
@@ -5,20 +5,97 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('citizen-engagement', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+        // This is the abstract state for the tabs directive.
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+
+            // The three next states are for each of the three tabs.
+            // The state names start with "tab.", indicating that they are children of the "tab" state.
+            .state('tab.newIssue', {
+                // The URL (here "/newIssue") is used only internally with Ionic; you never see it displayed anywhere.
+                // In an Angular website, it would be the URL you need to go to with your browser to enter this state.
+                url: '/newIssue',
+                views: {
+                    // The "tab-newIssue" view corresponds to the <ion-nav-view name="tab-newIssue"> directive used in the tabs.html template.
+                    'tab-newIssue': {
+                        // This defines the template that will be inserted into the directive.
+                        templateUrl: 'templates/newIssue.html'
+                    }
+                }
+            })
+
+            .state('tab.issueMap', {
+                url: '/issueMap',
+                views: {
+                    'tab-issueMap': {
+                        templateUrl: 'templates/issueMap.html'
+                    }
+                }
+            })
+
+            .state('tab.issueList', {
+                url: '/issueList',
+                views: {
+                    'tab-issueList': {
+                        templateUrl: 'templates/issueList.html'
+                    }
+                }
+            })
+
+            // This is the issue details state.
+            .state('tab.issueDetails', {
+                // We use a parameterized route for this state.
+                // That way we'll know which issue to display the details of.
+                url: '/issueDetails/:issueId',
+                views: {
+                    // Here we use the same "tab-issueList" view as the previous state.
+                    // This means that the issue details template will be displayed in the same tab as the issue list.
+                    'tab-issueList': {
+                        templateUrl: 'templates/issueDetails.html'
+                    }
+                }
+            })
+
+
+            // pas sur si ce code va ici ... a controler
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html'
+            })
+        ;
+
+        // Define the default state (i.e. the first screen displayed when the app opens).
+        $urlRouterProvider.otherwise(function ($injector) {
+            $injector.get('$state').go('tab.newIssue'); // Go to the new issue tab by default.
+        });
+    })
