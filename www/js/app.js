4000cf8bc942a353fbea0b81c915a8a480d53d24
U-SABINEROCHAT\Sabine <sabine.rochat@heig-vd.ch>
2016-04-04 20:46:40 +0200

    test

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('citizen-engagement', ['citizen-engagement.auth', 'ionic', 'citizen-engagement.constants'])
+angular.module('citizen-engagement', ['citizen-engagement.auth  ', 'ionic', 'citizen-engagement.constants'])
 
 
 
@@ -19,7 +19,6 @@ angular.module('citizen-engagement', ['citizen-engagement.auth', 'ionic', 'citiz
         controller: 'LoginCtrl',
         templateUrl: 'templates/login.html'
     })
-
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
