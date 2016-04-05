6fa96e95c9675f8aa2052be6ba869d8ea41b9285
U-SABINEROCHAT\Sabine <sabine.rochat@heig-vd.ch>
2016-04-05 11:11:15 +0200

    login

--- www/js/auth.js
+++ www/js/auth.js
@@ -5,39 +5,38 @@
 //copied from tutorial
 angular.module('citizen-engagement.auth', ['angular-storage'])
 
-    .factory('AuthService', function (store) {
+    .factory('AuthService', function() {
 
         var service = {
-            currentUserId: store.get('currentUserId'),
+            currentUserId: null,
 
-            setUser: function (user) {
+            setUser: function(user) {
                 service.currentUserId = user.userId;
-                store.set('currentUserId', user.userId);
             },
 
-            unsetUser: function () {
+            unsetUser: function() {
                 service.currentUserId = null;
-                store.remove('currentUserId');
             }
-
         };
 
         return service;
     })
 
 
+
+
     //copied from tutorial
-    .controller('LoginCtrl', function (apiUrl, AuthService, $http, $ionicHistory, $ionicLoading, $scope, $state) {
+    .controller('LoginCtrl', function(apiUrl, AuthService, $http, $ionicHistory, $ionicLoading, $scope, $state) {
 
         // The $ionicView.beforeEnter event happens every time the screen is displayed.
-        $scope.$on('$ionicView.beforeEnter', function () {
+        $scope.$on('$ionicView.beforeEnter', function() {
             // Re-initialize the user object every time the screen is displayed.
             // The first name and last name will be automatically filled from the form thanks to AngularJS's two-way binding.
             $scope.user = {};
         });
 
         // Add the register function to the scope.
-        $scope.register = function () {
+        $scope.register = function() {
 
             // Forget the previous error (if any).
             delete $scope.error;
@@ -51,9 +50,9 @@ angular.module('citizen-engagement.auth', ['angular-storage'])
             // Make the request to retrieve or create the user.
             $http({
                 method: 'POST',
-                url: apiUrl+'/users/logister',
+                url: 'http://localhost:8100/api-proxy/users/logister',
                 data: $scope.user
-            }).success(function (user) {
+            }).success(function(user) {
 
                 // If successful, give the user to the authentication service.
                 AuthService.setUser(user);
@@ -69,9 +68,9 @@ angular.module('citizen-engagement.auth', ['angular-storage'])
                 });
 
                 // Go to the issue creation tab.
-                $state.go('tab.newIssue'); //TODO change the redirection with if checking staff & citizen
+                $state.go('tab.newIssue');
 
-            }).error(function () {
+            }).error(function() {
 
                 // If an error occurs, hide the loading message and show an error message.
                 $ionicLoading.hide();
@@ -80,8 +79,8 @@ angular.module('citizen-engagement.auth', ['angular-storage'])
         };
     })
 
-    .controller('LogoutCtrl', function (AuthService, $scope, $state) {
-        $scope.logOut = function () {
+    .controller('LogoutCtrl', function(AuthService, $scope, $state) {
+        $scope.logOut = function() {
             AuthService.unsetUser();
             $state.go('login');
         };
@@ -89,12 +88,12 @@ angular.module('citizen-engagement.auth', ['angular-storage'])
 
 
     //copied from tutorial
-    .factory('AuthInterceptor', function (AuthService) {
+    .factory('AuthInterceptor', function(AuthService) {
         return {
 
             // The request function will be called before all requests.
             // In it, you can modify the request configuration object.
-            request: function (config) {
+            request: function(config) {
 
                 // If the user is logged in, add the X-User-Id header.
                 if (AuthService.currentUserId) {
@@ -108,6 +107,25 @@ angular.module('citizen-engagement.auth', ['angular-storage'])
 
     .config(function($httpProvider) {
         $httpProvider.interceptors.push('AuthInterceptor');
+    })
+
+    .service('AuthService', function(store) {
+
+        var service = {
+            currentUserId: store.get('currentUserId'),
+
+            setUser: function(user) {
+                service.currentUserId = user.userId;
+                store.set('currentUserId', user.userId);
+            },
+
+            unsetUser: function() {
+                service.currentUserId = null;
+                store.remove('currentUserId');
+            }
+        };
+
+        return service;
     })
 
 ;
