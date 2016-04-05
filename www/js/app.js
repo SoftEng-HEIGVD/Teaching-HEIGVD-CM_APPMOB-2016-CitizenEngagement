b0dfb7b638151803407ea5473670b736830162fb
patrick <chhen.patrick@gmail.com>
2016-04-05 16:05:08 +0200

    App
    Pas fonctionnel sans ma partie de test du newIssue.html

--- www/js/app.js
+++ www/js/app.js
@@ -5,129 +5,175 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('FixYourStreet', ['ionic', 'FixYourStreet.auth', 'FixYourStreet.constants'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        .run(function ($ionicPlatform) {
+            $ionicPlatform.ready(function () {
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                    // for form inputs)
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+                    // Don't remove this line unless you know what you are doing. It stops the viewport
+                    // from snapping when text inputs are focused. Ionic handles this internally for
+                    // a much nicer keyboard experience.
+                    cordova.plugins.Keyboard.disableScroll(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        })
 
-.config(function($stateProvider, $urlRouterProvider) {
+        .config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+            // Ionic uses AngularUI Router which uses the concept of states
+            // Learn more here: https://github.com/angular-ui/ui-router
+            // Set up the various states which the app can be in.
+            // Each state's controller can be found in controllers.js
+            $stateProvider
 
-    .state('issueMap', {
-      url: '/issueMap',
-      templateUrl: 'templates/issueMap.html'
-    })
+                    .state('issueMap', {
+                        url: '/issueMap',
+                        templateUrl: 'templates/issueMap.html'
+                    })
 
-    .state('newIssue', {
-      url: '/newIssue',
-      templateUrl: 'templates/newIssue.html'
-    })
+                    .state('newIssue', {
+                        url: '/newIssue',
+                        templateUrl: 'templates/newIssue.html'
+                    })
 
-    .state('issueDetails', {
-      url: '/issueDetails',
-      templateUrl: 'templates/issueDetails.html'
-    })
+                    .state('issueDetails', {
+                        url: '/issueDetails',
+                        templateUrl: 'templates/issueDetails.html'
+                    })
 
-    .state('issueList', {
-      url: '/issueList',
-      templateUrl: 'templates/issueList.html'
-    })
-
-
-    .state('login', {
-      url: '/login',
-      controller: 'LoginCtrl',
-      templateUrl: 'templates/login.html'
-    })
-
-  ;
+                    .state('issueList', {
+                        url: '/issueList',
+                        templateUrl: 'templates/issueList.html'
+                    })
 
 
+                    .state('login', {
+                        url: '/login',
+                        controller: 'LoginCtrl',
+                        templateUrl: 'templates/login.html'
+                    })
+
+                    ;
 
 
-  // Define the default state (i.e. the first screen displayed when the app opens).
-  $urlRouterProvider.otherwise(function($injector) {
-    $injector.get('$state').go('issueMap'); // Go to the new issue tab by default.
-  });
-})
 
-.run(function(AuthService, $rootScope, $state) {
 
-  // Listen for the $stateChangeStart event of AngularUI Router.
-  // This event indicates that we are transitioning to a new state.
-  // We have the possibility to cancel the transition in the callback function.
-  $rootScope.$on('$stateChangeStart', function(event, toState) {
+            // Define the default state (i.e. the first screen displayed when the app opens).
+            $urlRouterProvider.otherwise(function ($injector) {
+                $injector.get('$state').go('issueMap'); // Go to the new issue tab by default.
+            });
+        })
 
-    // If the user is not logged in and is trying to access another state than "login"...
-    if (!AuthService.currentUserId && toState.name != 'login') {
+        .run(function (AuthService, $rootScope, $state) {
 
-      // ... then cancel the transition and go to the "login" state instead.
-      event.preventDefault();
-      $state.go('login');
-    }
-  });
-})
+            // Listen for the $stateChangeStart event of AngularUI Router.
+            // This event indicates that we are transitioning to a new state.
+            // We have the possibility to cancel the transition in the callback function.
+            $rootScope.$on('$stateChangeStart', function (event, toState) {
 
-.controller("TagsCtrl", function ($scope) {
-    $scope.inputs = [{
-        value: null
-    }];
+                // If the user is not logged in and is trying to access another state than "login"...
+                if (!AuthService.currentUserId && toState.name != 'login') {
 
-    $scope.addInput = function () {
-        console.log("new input");
-        $scope.inputs.push({
-            value: null
-        });
-    }
+                    // ... then cancel the transition and go to the "login" state instead.
+                    event.preventDefault();
+                    $state.go('login');
+                }
+            });
+        })
 
-    $scope.removeInput = function (index) {
-        $scope.inputs.splice(index, 1);
-    }
-})
+        .controller("IssueCtrl", function (apiUrl, $scope, $http) {
+            $scope.inputs = [{
+                    value: null
+                }];
 
-.controller('ListCtrl', function($scope) {
-  $scope.groups = [];
-  for (var i=0; i<10; i++) {
-    $scope.groups[i] = {
-      name: i,
-      items: []
-    };
-    for (var j=0; j<3; j++) {
-      $scope.groups[i].items.push(i + '-' + j);
-    }
-  }
+            $scope.addInput = function () {
+                console.log("new input");
+                $scope.inputs.push({
+                    value: null
+                });
+            }
 
-  /*
-   * if given group is the selected group, deselect it
-   * else, select the given group
-   */
-  $scope.toggleGroup = function(group) {
-    if ($scope.isGroupShown(group)) {
-      $scope.shownGroup = null;
-    } else {
-      $scope.shownGroup = group;
-    }
-  };
-  $scope.isGroupShown = function(group) {
-    return $scope.shownGroup === group;
-  };
+            $scope.submitIssue = function () {
+//                var date = $scope.date = new Date();
+//                console.log(date);
+                var newIssue = {
+                    description: $scope.description,
+                    tags: [$scope.tagValue],
+                    lat: "46.780806678050126",
+                    lng: "6.630673501428493",
+                    imageUrl: "https://departmentfortransport.files.wordpress.com/2014/02/bb2-vzyimaaw3cx-large.jpg",
+                }
+                $http({
+                    method: 'POST',
+                    url: apiUrl + '/issues',
+                    data: newIssue
+                })
+                        .success(function (createdIssue) {
+                            console.log(createdIssue);
+                        });
+            }
 
-})
+            $scope.removeInput = function (index) {
+                $scope.inputs.splice(index, 1);
+            }
+
+        })
+        
+        
+        
+        .controller("typeCtrl", function (apiUrl, $scope, $http) {
+            $scope.inputs = [{
+                    value: null
+                }];
+
+            $scope.submitType = function () {
+                $http({
+                    method: 'GET',
+                    url: apiUrl + '/issueType',
+                })
+                        .success(function (typeChoosen) {
+                            console.log(typeChoosenIssue);
+                        });
+            }
+
+
+            $scope.removeInput = function (index) {
+                $scope.inputs.splice(index, 1);
+            }
+
+        })
+
+
+        .controller('ListCtrl', function ($scope) {
+            $scope.groups = [];
+            for (var i = 0; i < 10; i++) {
+                $scope.groups[i] = {
+                    name: i,
+                    items: []
+                };
+                for (var j = 0; j < 3; j++) {
+                    $scope.groups[i].items.push(i + '-' + j);
+                }
+            }
+
+            /*
+             * if given group is the selected group, deselect it
+             * else, select the given group
+             */
+            $scope.toggleGroup = function (group) {
+                if ($scope.isGroupShown(group)) {
+                    $scope.shownGroup = null;
+                } else {
+                    $scope.shownGroup = group;
+                }
+            };
+            $scope.isGroupShown = function (group) {
+                return $scope.shownGroup === group;
+            };
+
+        })
