3c5a8df011d35ab532430ec57fb2c6036da5a4bf
Michael Zwyssig <mzwyssig@3ne.ch>
2016-04-05 18:33:59 +0200

    new issue works and redirects to issue list without the back button

--- www/js/issues.js
+++ www/js/issues.js
@@ -8,9 +8,13 @@ angular.module('citizen-engagement')
         $scope.getIssues = function() {
             $http({
                 method: 'GET',
-                url: apiUrl + '/issues'
+                url: apiUrl + '/issues',
+                headers: {
+                    "x-sort": "-createdOn",
+                }
             }).success(function (issues) {
                 $scope.issues = issues;
+                console.log(issues);
             }).error(function(error) {
                 console.log('an error occurred');
             }).finally(function() {
@@ -22,7 +26,12 @@ angular.module('citizen-engagement')
     })
 
     // Get specific issue
-    .controller('GetSpecificIssueCtrl', function(apiUrl, $scope, $http,$stateParams) {
+    .controller('GetSpecificIssueCtrl', function(apiUrl, $scope, $state, $http, $stateParams, $ionicHistory) {
+
+        /*if($ionicHistory.backView() && $ionicHistory.backView().stateName == 'mainMenu.newIssue') {
+            $ionicHistory.clearHistory();
+            //TODO: Add something to remove back button
+        };*/
         $http({
             method: 'GET',
             url: apiUrl + '/issues/'+$stateParams.issueId
@@ -37,12 +46,41 @@ angular.module('citizen-engagement')
 
     })
     // Add new issue
-    .controller('AddIssueCtrl', function(apiUrl, $scope, $http) {
+    .controller('AddIssueCtrl', function(apiUrl, $scope, $http, $state, $ionicHistory) {
+
+        $scope.issue = {};
+
+        $http({
+            method: 'GET',
+            url: apiUrl + '/issueTypes'
+        }).success(function (data) {
+            $scope.issueTypes = data;
+        }).error(function(error) {
+            console.log('Could not get types');
+        });
+
+
+        $scope.sendIssue = function() {
+            $ionicHistory.nextViewOptions(
+                {
+                    disableBack: true
+                });
+            $http({
+                method: 'POST',
+                url: apiUrl + '/issues',
+                data: $scope.issue
+            }).success(function (data) {
+                $state.go('mainMenu.issueDetails', {issueId: data.id});
+            }).error(function(error){
+                console.log("error...");
+            });
+        }
 
     })
 
-    .controller('ListMyIssuesCtrl', function(apiUrl, $scope, $http) {
-        // Create and execute get functions
+    .controller('ListMyIssuesCtrl', function( apiUrl, $scope, $http) {
+
+
         $scope.getIssues = function() {
             $http({
                 method: 'GET',
@@ -50,7 +88,7 @@ angular.module('citizen-engagement')
             }).success(function (issues) {
                 $scope.issues = issues;
             }).error(function(error) {
-                console.log('an error occurred');
+                console.log(error);
             }).finally(function() {
                 // Stop the ion-refresher from spinning
                 $scope.$broadcast('scroll.refreshComplete');
