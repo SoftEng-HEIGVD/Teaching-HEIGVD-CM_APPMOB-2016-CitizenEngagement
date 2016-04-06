18fcf887892d9ac9562d4f5534c9a2607b878702
Oreonax <esok@mailoo.org>
2016-04-06 13:42:36 +0200

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('FixYourStreet', ['ionic', 'FixYourStreet.auth', 'FixYourStreet.constants', 'FixYourStreet.listIssue'])
+angular.module('FixYourStreet', ['ionic', 'FixYourStreet.auth', 'leaflet-directive', 'FixYourStreet.constants', 'FixYourStreet.listIssue'])
 
         .run(function ($ionicPlatform) {
             $ionicPlatform.ready(function () {
@@ -167,34 +167,6 @@ angular.module('FixYourStreet', ['ionic', 'FixYourStreet.auth', 'FixYourStreet.c
 
         })
 
-        .controller('ListCtrl', function ($scope) {
-            $scope.groups = [];
-            for (var i = 0; i < 10; i++) {
-                $scope.groups[i] = {
-                    name: i,
-                    items: []
-                };
-                for (var j = 0; j < 3; j++) {
-                    $scope.groups[i].items.push(i + '-' + j);
-                }
-            }
-
-            /*
-             * if given group is the selected group, deselect it
-             * else, select the given group
-             */
-            $scope.toggleGroup = function (group) {
-                if ($scope.isGroupShown(group)) {
-                    $scope.shownGroup = null;
-                } else {
-                    $scope.shownGroup = group;
-                }
-            };
-            $scope.isGroupShown = function (group) {
-                return $scope.shownGroup === group;
-            };
-
-        })
 
         .controller("TagsCtrl", function ($scope) {
             $scope.inputs = [{
