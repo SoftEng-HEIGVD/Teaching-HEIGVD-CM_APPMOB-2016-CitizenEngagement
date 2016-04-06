f5d6f0115176f7ae0523f42f9bf422164e2e2fd5
Gregoire Meylan <gmeylan@gmail.com>
2016-04-06 15:10:15 +0200

    Worked on the code

--- www/js/issues.js
+++ www/js/issues.js
@@ -1,36 +1,39 @@
 angular.module('citizen-engagement')
 
 .controller("allIssuesCtrl", function($scope, $http, apiUrl) {
-    $scope.issues = [];
-    $scope.scrollEnable = true;
 
-    var currentPage = 0;
-    $scope.loadMoreIssues = function() {
+    $scope.noMoreItemsAvailable = false;
+    $scope.issues = [];
+    var i = 0;
+    var currentPage = 0
+
+    $scope.loadMore = function() {
+
+        i++;
         $http({
             method: 'GET',
             url: apiUrl + '/issues',
             headers: {
-                'x-pagination': currentPage + ";5"
+                'x-pagination': currentPage + ";1"
             }
         }).success(function(issues) {
-            console.log(issues.length)
-            if(issues.length == 0){
-                $scope.scrollEnable = false;
-                console.log($scope.scrollEnable);
-            }
             $scope.issues = $scope.issues.concat(issues);
-            });
+        });
 
         currentPage++;
-    }
 
-
+        if ( i == 14 ) {
+            $scope.noMoreItemsAvailable = true;
+        }
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+    };
 })
 
-.controller("issueCtrl", function($scope, $http, apiUrl) {
+.controller("issueCtrl", function($scope, $http, apiUrl, $stateParams) {
+        var issueId = $stateParams.issueId;
         $http({
             method: 'GET',
-            url: apiUrl + '/issues/'
+            url: apiUrl + '/issues/'+issueId
         }).success(function(issue) {
 
             $scope.issue = issue;
