e76a83fc140c623a6d2a03d55dac03de274f36ce
Quentin Schwitzguébel <negus1309@gmail.com>
2016-04-06 08:34:46 +0200

    newIss

--- www/js/newIssue.js
+++ www/js/newIssue.js
@@ -1,6 +1,56 @@
-myApp.controller("IssueController", function($scope) {
+angular.module('citizen-engagement.newIssue',['ngTagsInput'])
 
-  
+
+.controller("newIssueCTRL", function($scope, $http, apiUrl) {
+  console.log(apiUrl)
+
+  //$scope.$on('$ionicView.beforeEnter', function() {
+
+    $http({
+      method: 'GET',
+      url: apiUrl + '/issueTypes'
+      //data: $scope.issueTypes
+
+    }).success(function(data) {
+      console.log(data)
+      $scope.issueTypes = data;
+
+    })
+
+
+
+
+
+
+
+      $scope.createAnIssue = function(){
+
+        console.log($scope.myIssueToPost)
+        /*var myIssueToPost = {}
+        myIssueToPost.description = $scope.description;
+
+        console.log(myIssueToPost.description)*/
+
+
+
+
+        /*$http({
+          method: 'POST',
+          url: apiUrl + '/issue'
+          //data: $scope.issueTypes
+
+        }).success(function(data) {
+          console.log(data)
+          $scope.issueTypes = data;
+
+        })*/
+
+    }
+
+
+  //})
+
+
 
 
 
