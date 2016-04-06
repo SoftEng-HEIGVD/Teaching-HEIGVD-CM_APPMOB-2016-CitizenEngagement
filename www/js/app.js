be40c1849ae0af9c6fc5327e48a325f7ce2f6b61
Flavia Pittet <pittet.flavia@gmail.com>
2016-04-06 09:21:29 +0200

    issue ctrl

--- www/js/app.js
+++ www/js/app.js
@@ -90,7 +90,8 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
                     // The "tab-newIssue" view corresponds to the <ion-nav-view name="tab-newIssue"> directive used in the tabs.html template.
                     'menuContent': {
                         // This defines the template that will be inserted into the directive.
-                        templateUrl: 'templates/newIssue.html'
+                        templateUrl: 'templates/newIssue.html',
+                        controller: 'NewIssueCtrl'
                     }
                 }
             })
--- www/js/newIssueCtrl.js
+++ www/js/newIssueCtrl.js
@@ -1,13 +1,13 @@
 angular.module('citizen-engagement.newIssueCtrl', [])
-    .controller('NewIssueListCtrl',
+    .controller('NewIssueCtrl',
         function ($scope, $http, apiUrl) {
             $scope.loadIssueTypes = function () {
                 console.log("ok");
-                $http.get(apiUrl + '/issueTypes').success(function (issues) {
+                $http.get(apiUrl + '/issueTypes').success(function (issueTypes) {
 
                     $scope.issueTypes = issueTypes;
 
-                    console.log(issues[0]);
+                    console.log(issueTypes[0]);
 
                 });
             };
