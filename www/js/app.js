da264c81f38401017c0a1d59fc20a35d4750017e
Oreonax <esok@mailoo.org>
2016-04-06 10:05:08 +0200

    Added other module

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('FixYourStreet', ['ionic', 'FixYourStreet.auth', 'FixYourStreet.constants'])
+angular.module('FixYourStreet', ['ionic', 'FixYourStreet.auth', 'FixYourStreet.constants', 'FixYourStreet.listIssue'])
 
         .run(function ($ionicPlatform) {
             $ionicPlatform.ready(function () {
@@ -133,7 +133,7 @@ angular.module('FixYourStreet', ['ionic', 'FixYourStreet.auth', 'FixYourStreet.c
                             console.log(createdIssue);
                         },
                                 function error() {
-                                    console.log("Erreur à faire");
+                                    console.log("Erreur ï¿½ faire");
                                 });
 
 
