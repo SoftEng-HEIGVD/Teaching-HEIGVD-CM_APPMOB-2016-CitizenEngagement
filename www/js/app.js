4675d33f15a868e7ef9a645ddd21dfb9bc5816b5
Gregoire Meylan <gmeylan@gmail.com>
2016-04-07 13:48:45 +0200

    map.js added

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citizen-engagement.constants', 'infinite-scroll','geolocation'])
+angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citizen-engagement.constants', 'infinite-scroll','geolocation', 'leaflet-directive'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -57,7 +57,8 @@ angular.module('citizen-engagement', ['ionic', 'citizen-engagement.auth', 'citiz
       url: '/issueMap',
       views: {
         'tab-issueMap': {
-          templateUrl: 'templates/issueMap.html'
+          templateUrl: 'templates/issueMap.html',
+          controller: 'MapController'
         }
       }
     })
---
+++ www/js/map.js
@@ -0,0 +1,15 @@
+angular.module('citizen-engagement')
+
+    .controller("MapController", function($scope, mapboxMapId, mapboxAccessToken) {
+        var mapboxTileLayer = "http://api.tiles.mapbox.com/v4/" + mapboxMapId;
+        mapboxTileLayer = mapboxTileLayer + "/{z}/{x}/{y}.png?access_token=" + mapboxAccessToken;
+        $scope.mapDefaults = {
+            tileLayer: mapboxTileLayer
+        };
+        $scope.mapCenter = {
+            lat: 51.48,
+            lng: 0,
+            zoom: 14
+        };
+        $scope.mapMarkers = [];
+    });
