e6926e9faf178de38f3e2d7d0db121830ae344e7
Joel Pinheiro De Almeida <Joel Pinheiro De Almeida>
2016-04-07 13:50:26 +0200

    youpi

--- www/js/citizenCtrl.js
+++ www/js/citizenCtrl.js
@@ -1,7 +1,7 @@
-angular.module('citizen-engagement.citizenCtrl',[])
+angular.module('citizen-engagement.citizenCtrl',['geolocation'])
 
     .controller('IssueListCtrl',
-        function ($scope, $http,apiUrl) {
+        function ($scope, $http,apiUrl,geolocation) {
 
             $scope.loadUsers = function() {
                 $http.get(apiUrl+'/users').success(function(users) {
@@ -30,10 +30,51 @@ angular.module('citizen-engagement.citizenCtrl',[])
                 $http.get(apiUrl+'/issues').success(function(issues){
                     $scope.issues=issues;
                 })
+            };
+
+            $scope.submitIssue= function(issue){
+
+                geolocation.getLocation().then(function(data){
+                    issue.lat=data.coords.latitude;
+                    issue.lng=data.coords.longitude;
+
+                    console.log(issue);
+
+
+                    $http({
+                        method: 'POST',
+                        url: 'https://heigvd-cm-appmob-2016-team-04.herokuapp.com/api',
+                        data: "message=" + message,
+                        headers: {'Content-Type': 'application/json'}
+                    });
+
+                })
+
+
+
+     /*         $scope.lng.myIssueLng;
+                $scope.lat.myIssueLat;
+                $scope.imageUrl.myIssueUrl;
+                $scope.issueTypeId.myIssueTypeId,*/
+/*
+
+                    $scope.pushNewIssue=function(){
+                        $http.post(apiUrl+'/issues/').success(function(issueCurrent){
+
+                        })
+                    }*/
+
+
+
             }
+
+
+
+
 
             $scope.loadIssues();
             $scope.loadIssueType();
+
 
         })
 
