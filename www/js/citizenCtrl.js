3e56cad4daff336b85ecd244b2a9037fe300cf40
Joel Pinheiro De Almeida <Joel Pinheiro De Almeida>
2016-04-07 09:43:25 +0200

    blabla

--- www/js/citizenCtrl.js
+++ www/js/citizenCtrl.js
@@ -1,64 +1,64 @@
 angular.module('citizen-engagement.citizenCtrl',[])
 
-.controller('IssueListCtrl',
-function ($scope, $http,apiUrl) {
+    .controller('IssueListCtrl',
+        function ($scope, $http,apiUrl) {
 
-    $scope.loadUsers = function() {
-        $http.get(apiUrl+'/users').success(function(users) {
+            $scope.loadUsers = function() {
+                $http.get(apiUrl+'/users').success(function(users) {
 
-            $scope.users = users;
+                    $scope.users = users;
 
-        });
-    };
+                });
+            };
 
-    $scope.loadIssueType = function () {
-        $http.get(apiUrl + '/issueTypes').success(function (issueTypes) {
-            $scope.issueTypes = issueTypes;
-            //console.log(issueTypes[0].name);
+            $scope.loadIssueType = function () {
+                $http.get(apiUrl + '/issueTypes').success(function (issueTypes) {
+                    $scope.issueTypes = issueTypes;
+                    //console.log(issueTypes[0].name);
 
-            index = 0;
-            while (index < issueTypes.length)
-            {
-                console.log(issueTypes[index].name);
-                index++;
+                    index = 0;
+                    while (index < issueTypes.length)
+                    {
+                        console.log(issueTypes[index].name);
+                        index++;
+                    }
+
+                });
+
+
+            };
+
+            $scope.loadIssues=function(){
+                $http.get(apiUrl+'/issues').success(function(issues){
+                    $scope.issues=issues;
+
+                })
             }
 
-        });
-
-
-    };
-
-    $scope.loadIssues=function(){
-        $http.get(apiUrl+'/issues').success(function(issues){
-            $scope.issues=issues;
+            $scope.loadIssues();
+            $scope.loadIssueType();
 
         })
-    }
-
-    $scope.loadIssues();
-    $scope.loadIssueType();
-
-})
 
 
 
-.controller('PagesCtrl', function ($scope, $stateParams,$http,apiUrl) {
+    .controller('PagesCtrl', function ($scope, $stateParams,$http,apiUrl) {
 
-        var currentId = $stateParams.issueId;
+            var currentId = $stateParams.issueId;
 
-        $scope.loadCurrentIssue=function(){
-            $http.get(apiUrl+'/issues/'+currentId).success(function(issueCurrent){
-                $scope.issueCurrent=issueCurrent;
+            $scope.loadCurrentIssue=function(){
+                $http.get(apiUrl+'/issues/'+currentId).success(function(issueCurrent){
+                    $scope.issueCurrent=issueCurrent;
 
-                console.log(issueCurrent);
+                    console.log(issueCurrent);
 
-            })
+                })
+            }
+
+            $scope.loadCurrentIssue();
         }
 
-        $scope.loadCurrentIssue();
-    }
-
-)
+    )
 
     .controller('MapController', function($log,$scope,geolocation){
 
@@ -77,11 +77,9 @@ function ($scope, $http,apiUrl) {
             $scope.issueTypeId.myIssueTypeId,
 
 
-            $scope.pushNewIssue=function(){
+                $scope.pushNewIssue=function(){
                     $http.post(apiUrl+'/issues/').success(function(issueCurrent){
 
                     })
                 }
-            })
-
-
+        })
