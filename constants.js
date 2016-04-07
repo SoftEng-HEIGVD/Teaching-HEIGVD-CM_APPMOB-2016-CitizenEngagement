85d8e4a65b5fa18cc6ec900c51bdcd3f3c4b8817
Tabata <thyppol@gmail.com>
2016-04-07 11:21:36 +0200

    set up camera plugin

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 angular.module("citizen-engagement.constants", [])
     .constant("apiUrl", "@apiUrl@")
-
     .constant("mapboxMapId", "@mapboxMapId@")
-    .constant("mapboxAccessToken", "@mapboxAccessToken@") ;
+    .constant("mapboxAccessToken", "@mapboxAccessToken@")
+    .constant("qimgUrl", "@qimgUrl@").constant("qimgToken", "@qimgToken@");
