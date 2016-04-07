14af4fe6783c6e4189987f92454f1576ae3a358e
Tabata <thyppol@gmail.com>
2016-04-07 09:59:18 +0200

    cameractrl

--- www/js/cameraCtrl.js
+++ www/js/cameraCtrl.js
@@ -1,1 +1,42 @@
 angular.module('citizen-engagement.cameraCtrl',[])
+
+.factory("CameraService", function($q) {
+  return {
+  getPicture: function(options) {
+    var deferred = $q.defer();
+    navigator.camera.getPicture(function(result) {
+      // do any magic you need
+      deferred.resolve(result);
+      }, function(err) {
+      deferred.reject(err);
+      }, options);
+        return deferred.promise;
+    }
+  }
+});
+
+.controller("cameraController", function(CameraService, $http, qimgUrl, qimgToken) {
+  // take the picture
+  CameraService.getPicture({
+    quality: 75,
+    targetWidth: 400,
+    targetHeight: 300,
+    // return base64-encoded data instead of a file
+    destinationType: Camera.DestinationType.DATA_URL
+    }).then(function(imageData) {
+      // upload the image
+      $http({
+      method: "POST",
+      url: qimgUrl + "/images",
+      headers: {
+      Authorization: "Bearer " + qimgToken
+      },
+      data: {
+      data: imageData
+    }
+    }).success(function(data) {
+      var imageUrl = data.url;
+      // do something with imageUrl
+    });
+  });
+})
